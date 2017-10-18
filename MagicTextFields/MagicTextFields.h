17f51caa1ea65e542374c2ba968781e36ab5c4e6
Felipe Ricieri <felipericieri@icloud.com>
2017-10-18 14:33:56 -0200

    Setup

---
+++ MagicTextFields/MagicTextFields.h
@@ -0,0 +1,19 @@
+//
+//  MagicTextFields.h
+//  MagicTextFields
+//
+//  Created by Felipe Ricieri on 18/10/17.
+//  Copyright © 2017 Ricieri ME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagicTextFields.
+FOUNDATION_EXPORT double MagicTextFieldsVersionNumber;
+
+//! Project version string for MagicTextFields.
+FOUNDATION_EXPORT const unsigned char MagicTextFieldsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicTextFields/PublicHeader.h>
+
+
